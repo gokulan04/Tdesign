516d686ef5625fcd27d91d70077fbd19c8cf98b7
GokulanGM <gokulan@admindroid.com>
2025-12-28 23:26:20 +0530

    next() callback added

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -8,47 +8,55 @@ const generateToken = (userId) => {
     });
 };
 
-export const register = async (req, res) => {
-    console.log("REQUEST", req.body);
-    const { name, email, password } = req.body;
+export const register = async (req, res, next) => {
+    try {
+        console.log("REQUEST", req.body);
+        const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email })
-    if (existingUser) {
-        return res.status(400).json({ message: "User already exists" });
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name, email, password: hashedPassword
+        });
+
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            token,
+            user: { id: user._id, email: user.email }
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-        name, email, password: hashedPassword
-    });
-
-    const token = generateToken(user._id);
-
-    res.status(201).json({
-        token,
-        user: { id: user._id, email: user.email }
-    });
 };
 
-export const login = async (req, res) => {
-    const { email, password } = req.body;
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select("+password");
-    if (!user) {
-        return res.status(401).json({ message: "invalid email" });
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(401).json({ message: "invalid email" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid passowrd" });
+        }
+
+        const token = generateToken(user._id);
+
+        res.json({
+            token,
+            user: { id: user._id, email: user.email }
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-        return res.status(401).json({ message: "Invalid passowrd" });
-    }
-
-    const token = generateToken(user._id);
-
-    res.json({
-        token,
-        user: { id: user._id, email: user.email }
-    });
 
 }
--- server/src/middleware/errorMiddleware.js
+++ server/src/middleware/errorMiddleware.js
@@ -8,7 +8,6 @@ export const notFound = (req, res, next) => {
 // for Express to recognize it as an Error Handler.
 export const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    console.log('error handler hits');
     res.status(statusCode).json({
         message: err.message,
         stack: process.env.NODE_ENV === "production" ? null : err.stack
